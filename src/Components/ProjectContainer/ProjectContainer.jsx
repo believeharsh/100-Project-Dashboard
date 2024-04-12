908bde246515972d8ae452d0bb903ad4744f645e
Believeharsh <bontinue@gmail.com>
2024-04-12 22:13:34 +0530

    UI work has been completed

--- src/Components/ProjectContainer/ProjectContainer.jsx
+++ src/Components/ProjectContainer/ProjectContainer.jsx
@@ -3,7 +3,7 @@ import ProjectsList from "../ProjectsList/ProjectsList";
 const ProjectContainer = () => {
   return (
     <>
-      <div className=" bg-slate-900 h-full border-white border-[1px] rounded-xl scrollbar  w-2 flex-1 overflow-y-auto ml-[180px] ">
+      <div className=" bg-slate-900 h-full border-white border-[1px] rounded-l-xl  scrollbar  w-2 flex-1 overflow-y-auto ml-[180px] ">
         <div className="m-[10px]  ml-2 mt-2">
           <ProjectsList />
         </div>
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -2,46 +2,57 @@ import React from "react";
 import { FaLink } from "react-icons/fa";
 import { FaCode } from "react-icons/fa";
 
-const Projects = ({ count , name, image, link, codeLink }) => {
+const Projects = ({ count, name, image, link, codeLink }) => {
   return (
     <>
-    <div className="flex justify-between mx-auto text-white px-5 py-4 border-b-[1px] border-white text-2xl">
-      <div className="">{count}</div>
-      
-      <div className="flex justify-center ">
-        <div className="w-[110px] mx-1 ">{name}</div>
-      </div>
-      <div className="">
-        <img
-          className="w-[190px] h-[110px] rounded-sm"
-          src={`${image}`}
-          alt=""
-        />
-      </div>
-      <div className="flex flex-col ">
-        <a href={`${link}`} target="_blank">
-          LInk
-        </a>
-        <a className="mx-auto mt-2" href={`${link}`} target="_blank">
-          <FaLink />
-        </a>
-      </div>
-      <div className="flex flex-col ">
-        <a href={`${codeLink}`} target="_blank">
-          Code
-        </a>
-        <a
-          className="mx-auto mt-2"
-          href={`${codeLink}`}
-          target="_blank"
-        >
-          <FaCode />
-        </a>
-      </div>
-    </div>
+      <div className="flex justify-between mx-auto text-white px-5 py-4 text-2xl hover:border-gray-600 hover:border-[0.1px] hover:rounded-xl">
+        <div className="flex gap-[52px] cursor-pointer">
+          <div className="">{count + "."}</div>
 
+          {/* <div className="flex justify-center ">
+            <div className="w-[110px] mx-1 cursor-pointer">{name}</div>
+          </div> */}
+          <div class="relative flex justify-center ">
+            <span class=" z-10 w-[110px] mx-1 cursor-pointer group">
+              {name}
+              <div class="absolute inset-x-0  h-0.5 bg-white w-0 transition-all duration-300 group-hover:w-full"></div>
+            </span>
+          </div>
+        </div>
+
+        <div className="rounded-xl p-[5.5px] border-gray-500 border-double border-[1.8px]">
+          <img
+            className="w-[260px] h-[120px] rounded-sm "
+            src={`${image}`}
+            alt=""
+          />
+        </div>
+        <div className="flex gap-[52px]">
+          <div className="flex flex-col ">
+            <a href={`${link}`} target="_blank" className="relative">
+            <span class=" z-10 w-[110px] mx-1 cursor-pointer group">
+              Link
+              <div class="absolute inset-x-0  h-0.5 bg-white w-0 transition-all duration-300 group-hover:w-full"></div>
+            </span>
+            </a>
+            <a className="mx-auto mt-2" href={`${link}`} target="_blank">
+              <FaLink />
+            </a>
+          </div>
+          <div className="flex flex-col ">
+            <a href={`${codeLink}`} target="_blank" className="relative">
+            <span class=" z-10 w-[110px] mx-1 cursor-pointer group">
+              Code
+              <div class="absolute inset-x-0  h-0.5 bg-white w-0 transition-all duration-300 group-hover:w-full"></div>
+            </span>
+            </a>
+            <a className="mx-auto mt-2" href={`${codeLink}`} target="_blank">
+              <FaCode />
+            </a>
+          </div>
+        </div>
+      </div>
     </>
-    
   );
 };
 
