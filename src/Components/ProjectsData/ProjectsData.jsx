98bb982284dc5f76ed587b76c5e400c05d46c7d6
Believeharsh <bontinue@gmail.com>
2024-04-14 16:16:50 +0530

    added Project Data josn till 21 projects

--- src/Components/ProjectsData/ProjectsData.jsx
+++ src/Components/ProjectsData/ProjectsData.jsx
@@ -5,7 +5,7 @@ const ProjectData = [
       name: 'Tell-Me-Joke',
       image: "/100-Apps-Images/1-Tell-Me-Joke.png",
       link: 'https://replit.com/@believeharsh/Random-Joke-Generator',
-      codeLink: 'https://github.com/believeharsh/100-Days-100-Project/tree/master/Tell-me-Joke'
+      codeLink: 'https://github.com/believeharsh/100-Days-100-Project/tree/master/1-Tell-me-Joke'
     },
     {
       id: 2,
@@ -13,7 +13,7 @@ const ProjectData = [
       name: 'Random-Meel-Generate',
       image: "/100-Apps-Images/2-Random-Meel.png",
       link: 'https://random-meel-generator.netlify.app/',
-      codeLink: 'https://github.com/believeharsh/100-Days-100-Project/tree/master/Random-Meel-generator'
+      codeLink: 'https://github.com/believeharsh/100-Days-100-Project/tree/master/2-Random-Meel-generator'
     },
     {
       id: 3,
@@ -21,7 +21,7 @@ const ProjectData = [
       name: 'Tell-Me-Joke',
       image: "/100-Apps-Images/3-Mood-Calendar.png",
       link: 'https://replit.com/@believeharsh/Mood-Calander',
-      codeLink: 'https://github.com/believeharsh/100-Days-100-Project/tree/master/2023-Mood-Calender'
+      codeLink: 'https://github.com/believeharsh/100-Days-100-Project/tree/master/3-2023-Mood-Calender'
     },
     {
       id: 4,
@@ -29,7 +29,7 @@ const ProjectData = [
       name: 'Contact-form',
       image: "/100-Apps-Images/4-Contact-form.png",
       link: 'https://replit.com/@believeharsh/Contact-form',
-      codeLink: 'https://github.com/believeharsh/100-Days-100-Project/tree/master/Contact-page'
+      codeLink: 'https://github.com/believeharsh/100-Days-100-Project/tree/master/4-Contact-page'
     },
     {
       id: 5,
@@ -37,7 +37,7 @@ const ProjectData = [
       name: 'Countries-Details',
       image: "/100-Apps-Images/5-Country-Details.png",
       link: 'https://replit.com/@believeharsh/Countries-Details',
-      codeLink: 'https://github.com/believeharsh/100-Days-100-Project/tree/master/Country-Details'
+      codeLink: 'https://github.com/believeharsh/100-Days-100-Project/tree/master/5-Country-Details'
     },
     {
       id: 6,
@@ -45,7 +45,7 @@ const ProjectData = [
       name: 'To-Do-List',
       image: "/100-Apps-Images/6-To-Do-List.png",
       link: 'https://replit.com/@believeharsh/To-Do-List',
-      codeLink: 'https://github.com/believeharsh/100-Days-100-Project/tree/master/To-Do-List'
+      codeLink: 'https://github.com/believeharsh/100-Days-100-Project/tree/master/6-To-Do-List-JS'
     },
     {
       id: 7,
@@ -53,7 +53,7 @@ const ProjectData = [
       name: 'Cards-Generator',
       image: "/100-Apps-Images/7-Cards-Generator.png",
       link: 'https://replit.com/@believeharsh/Cards-Generator',
-      codeLink: 'https://github.com/believeharsh/100-Days-100-Project/tree/master/Cards-generator'
+      codeLink: 'https://github.com/believeharsh/100-Days-100-Project/tree/master/7-Cards-generator'
     },
     {
       id: 8,
@@ -61,7 +61,7 @@ const ProjectData = [
       name: 'PassWord-Generator',
       image: "/100-Apps-Images/8-PassWord-Generator.png",
       link: 'https://replit.com/@believeharsh/PassWord-Generator',
-      codeLink: 'https://github.com/believeharsh/100-Days-100-Project/tree/master/Password-Generator'
+      codeLink: 'https://github.com/believeharsh/100-Days-100-Project/tree/master/8-Password-Generator'
     },
     {
       id: 9,
@@ -69,7 +69,7 @@ const ProjectData = [
       name: 'Tumhare-Notes',
       image: "/100-Apps-Images/9-Tumhare-Notes.png",
       link: 'https://tumharenotes.netlify.app/',
-      codeLink: 'https://github.com/believeharsh/Tumhare-Notes'
+      codeLink: 'https://github.com/believeharsh/100-Days-100-Project/tree/master/9-Tumhare-Notes-App'
     },
     {
       id: 10,
@@ -77,7 +77,7 @@ const ProjectData = [
       name: '3D-Animation-Box',
       image: "/100-Apps-Images/10-3d-Animation-Box.png",
       link: 'https://replit.com/@believeharsh/3D-Box-Animation',
-      codeLink: 'https://github.com/believeharsh/100-Days-100-Project/tree/master/3D-Animation-Box'
+      codeLink: 'https://github.com/believeharsh/100-Days-100-Project/tree/master/10-3D-Animation-Box'
     },
     {
       id: 11,
@@ -85,7 +85,7 @@ const ProjectData = [
       name: 'React-Routing',
       image: "/100-Apps-Images/11-React-Routing.png",
       link: 'https://routing-react-reactjs.netlify.app',
-      codeLink: 'https://github.com/believeharsh/100-Days-100-Project/tree/master/React-Routing'
+      codeLink: 'https://github.com/believeharsh/100-Days-100-Project/tree/master/11-React-Routing'
     },
     {
       id: 12,
@@ -93,7 +93,7 @@ const ProjectData = [
       name: 'Theme-Switcher',
       image: "/100-Apps-Images/12-Theme-Switcher.png",
       link: 'https://context-api-theme-switcher.netlify.app/',
-      codeLink: 'https://github.com/believeharsh/100-Days-100-Project/tree/master/Theme-Switcher'
+      codeLink: 'https://github.com/believeharsh/100-Days-100-Project/tree/master/12-Theme-Switcher'
     },
     {
       id: 13,
@@ -101,7 +101,7 @@ const ProjectData = [
       name: 'Fruit-Stock Manager',
       image: "/100-Apps-Images/13-Fruit-Stock.png",
       link: 'https://fruite-stock-operator.netlify.app/',
-      codeLink: 'https://github.com/believeharsh/100-Days-100-Project/tree/master/ThinkInReact'
+      codeLink: 'https://github.com/believeharsh/100-Days-100-Project/tree/master/13-Fruit-Stock-Manager'
     },
     {
       id: 14,
@@ -109,7 +109,7 @@ const ProjectData = [
       name: 'BMI-Calculator',
       image: "/100-Apps-Images/14-BMI-Calculator.png",
       link: 'https://apna-bmi-calculator.netlify.app/',
-      codeLink: 'https://github.com/believeharsh/100-Days-100-Project/tree/master/BMI-Calculator'
+      codeLink: 'https://github.com/believeharsh/100-Days-100-Project/tree/master/14-BMI-Calculator'
     },
     {
       id: 15,
@@ -117,7 +117,7 @@ const ProjectData = [
       name: 'Twitter-Clone',
       image: "/100-Apps-Images/15-Twitter-Clone.png",
       link: 'https://twitterclonebytailwind.netlify.app/',
-      codeLink: 'https://github.com/believeharsh/100-Days-100-Project/tree/master/TwitterClone'
+      codeLink: 'https://github.com/believeharsh/100-Days-100-Project/tree/master/15-TwitterClone'
     },
     {
       id: 16,
@@ -125,16 +125,53 @@ const ProjectData = [
       name: 'ToDo-React-App',
       image: '/100-Apps-Images/16-ToDo-React-App.png',
       link: 'https://believeharsh-to-do-app.vercel.app/',
-      codelink: 'https://github.com/believeharsh/To-Do-App-In-React'
+      codelink: 'https://github.com/believeharsh/100-Days-100-Project/tree/master/16-ToDo-App-In-React'
     },
     {
       id: 17,
-      count : 17,
+      count:17,
+      name: 'Tic-Tac-Toe',
+      image: '/100-Apps-Images/16-ToDo-React-App.png',
+      link: '',
+      codelink: ''
+
+    },
+    {
+      id: 18,
+      count:18,
+      name: 'BookMarks-App',
+      image: '/100-Apps-Images/16-ToDo-React-App.png',
+      link: '',
+      codelink: ''
+    },
+
+    {
+      id: 19,
+      count : 19,
       name : 'React-Form-Firebase',
       image: '/100-Apps-Images/17-React-Form-Firebase.png',
       link : 'https://firebase-signup-page.netlify.app/',
       codelink : 'https://github.com/believeharsh/100-Days-100-Project/tree/master/19-React-Form-Firebase'
+    } ,
+    {
+      id: 20,
+      count:20,
+      name: 'Signup-Page',
+      image: '/100-Apps-Images/16-ToDo-React-App.png',
+      link: '',
+      codelink: ''
+
+    },
+    {
+      id: 21,
+      count:21,
+      name: 'Typing-Web-App',
+      image: '/100-Apps-Images/16-ToDo-React-App.png',
+      link: '',
+      codelink: ''
+
     }
+
     
     
   ]
